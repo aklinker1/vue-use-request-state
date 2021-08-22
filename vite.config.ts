6bfdb542f543e982e9da9b3534bb300a090ea171
Aaron Klinker <aaronklinker1@gmail.com>
2021-08-22 16:00:21 -0500

    fix: include correct types for `wrapRequest` and `RequestState`

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import typescript from 'rollup-plugin-typescript2';
-// @ts-expect-error: node types are not installed
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), typescript()],
+  plugins: [vue()],
   build: {
     lib: {
-      // @ts-expect-error: node types are not installed
       entry: path.resolve(__dirname, 'index.ts'),
       name: 'VueUseRequestState',
       fileName: 'index',
